15480f51f94f7ec58c0d1f35aa8f6f7c14de587a
valenblls18 <valenballada12@gmail.com>
2023-10-20 18:40:14 -0300

    agrego CRUD listar productos

--- Proyecto 2º Semestre/Backend/Controladores/ControladorProductos.py
+++ Proyecto 2º Semestre/Backend/Controladores/ControladorProductos.py
@@ -27,3 +27,25 @@ class ControladorProductos():
             producto = Producto(i[0],i[1],i[2],i[3],i[4],i[6],i[5],i[7])
             productos.append(producto)
         return productos
+    
+   consulta = "SELECT * FROM productos"
+   cursor = conexion.cursor()
+
+   cursor.execute(consulta)
+
+   resultados = cursor.fetchall()
+
+for fila in resultados:
+    idProductos, nombre, descripcion, pedido, stock_actual, proveedor, stock_min, categoria = fila
+    print(f"ID de Producto: {idProductos}")
+    print(f"Nombre: {nombre}")
+    print(f"Descripción: {descripcion}")
+    print(f"Pedido: {pedido}")
+    print(f"Stock Actual: {stock_actual}")
+    print(f"Proveedor: {proveedor}")
+    print(f"Stock Mínimo: {stock_min}")
+    print(f"Categoría: {categoria}")
+    print("\n")
+
+cursor.close()
+conexion.close()
