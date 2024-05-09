9c28bad9e6f0d0547bc2a459218eb33dac731f77
florCarrillo <florencianoelcarrillo@MacBook-Pro-de-Florencia.local>
2024-05-09 16:32:50 -0300

    Se crea componente Login en Angular y validaciones

--- Proyecto 2024/frontend/src/app/app.routes.ts
+++ Proyecto 2024/frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "login", component: LoginComponent}
+];
