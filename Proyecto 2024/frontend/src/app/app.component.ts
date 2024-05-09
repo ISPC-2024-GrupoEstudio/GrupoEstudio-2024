a2f902551fb7be9bba7fa137813f4e35a10c50b7
ValeAngeletti <valeangelett11@gmail.com>
2024-05-09 13:15:29 -0300

    Traspaso galeria y modificaciones css

--- Proyecto 2024/frontend/src/app/app.component.ts
+++ Proyecto 2024/frontend/src/app/app.component.ts
@@ -2,16 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './shared/nav/nav.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { DashboardComponent
-
- } from './pages/dashboard/dashboard.component';
+import { DashboardComponent} from './pages/dashboard/dashboard.component';
+import { ProductosComponent } from "./pages/productos/productos.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, NavComponent, FooterComponent, DashboardComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, NavComponent, FooterComponent, DashboardComponent, ProductosComponent]
 })
 export class AppComponent {
   title = 'frontend';
