10ed132e5c44ba2c11924a4531a9cd4fb6c570a5
[emiliano1191] <[emiliano1191@gmail.com]>
2024-05-09 22:57:44 -0300

    ac componente nav

--- Proyecto 2024/frontend/src/app/app.component.ts
+++ Proyecto 2024/frontend/src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './shared/nav/nav.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { DashboardComponent
-
- } from './pages/dashboard/dashboard.component';
+import { DashboardComponent} from './pages/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
--- Proyecto 2024/frontend/src/app/app.routes.ts
+++ Proyecto 2024/frontend/src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+{path:'',redirectTo:'/inicio', pathMatch:'full'},
+{path:'login',component: LoginComponent},
+{path:'contacto', component: ContactoComponent},
+{path:'productos', component: ProductosComponent},
+{path:'inicio', component:DashboardComponent}
+
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
