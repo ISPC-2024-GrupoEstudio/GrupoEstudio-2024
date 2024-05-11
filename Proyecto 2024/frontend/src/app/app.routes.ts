22f0cb5890584160b93c6dea8bbc2d5aa01616a7
ValeAngeletti <valeangelett11@gmail.com>
2024-05-11 18:08:45 -0300

    implementacion galeria de productos

--- Proyecto 2024/frontend/src/app/app.routes.ts
+++ Proyecto 2024/frontend/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegistroComponent } from './pages/auth/registro/registro.component';
 import { Component } from '@angular/core';
+import { ProductosComponent } from './pages/productos/productos.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
     {path: "registro", component:RegistroComponent},
+    {path:"galeria", component:ProductosComponent},
 ];
