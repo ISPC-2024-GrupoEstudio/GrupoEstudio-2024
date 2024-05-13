01b1e97478c5b883f792537506d47181003e6cd3
ValeAngeletti <valeangelett11@gmail.com>
2024-05-13 12:43:51 -0300

    Implementacion componentes estado envios

---
+++ Proyecto 2024/frontend/src/app/pages/dashboard/compra01/compra01.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-compra01',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './compra01.component.html',
+  styleUrl: './compra01.component.css'
+})
+export class Compra01Component {
+
+}
