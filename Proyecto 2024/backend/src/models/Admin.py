54de65255dfc95faefcdfc02495b70c3bcad8840
emir-argcoder <ea.samsam@gmail.com>
2024-05-13 19:42:13 -0300

    Se eliminan "src" y "server"

--- Proyecto 2024/backend/src/models/Admin.py
+++
@@ -1,5 +0,0 @@
-from .Usuario import Usuario
-
-class Admin(Usuario):
-    def __init__(self, nombre, nroDoc, tipoDoc, apellido, telefono, fechaAlta, direccion, fechaBaja, contraseña, nombreUsuario):
-        super().__init__(nombre, nroDoc, tipoDoc, apellido, telefono, fechaAlta, direccion, fechaBaja, contraseña, nombreUsuario)
