60c3dbcbb2520bf64e783eada011c0fe30b61d71
ValeAngeletti <valeangelett11@gmail.com>
2024-05-27 19:13:50 -0300

    Implementacion rutas hijas dashboard

--- Proyecto 2024/frontend/src/app/pages/dashboard/dashboard.component.ts
+++ Proyecto 2024/frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, Input } from '@angular/core';
+import { NavigationEnd, RouterLink, RouterOutlet, Router } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet, NgIf],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  showSections: boolean = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.showSections = event.urlAfterRedirects !== '/dashboard/historial-compras';
+      }
+    });
+  }
+
+  ngOnInit(): void {}
+
+  hideSections(): void {
+    this.showSections = false;
+  }
   
 }
---
+++ Proyecto 2024/frontend/src/app/pages/dashboard/historial-compras/historial-compras.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-historial-compras',
+  standalone: true,
+  imports: [],
+  templateUrl: './historial-compras.component.html',
+  styleUrl: './historial-compras.component.css'
+})
+export class HistorialComprasComponent {
+
+}
