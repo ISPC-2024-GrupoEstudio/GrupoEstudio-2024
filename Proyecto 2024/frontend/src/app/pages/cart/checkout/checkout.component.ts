2dbc922bd6395f1dd7ea67dea2b118b671fb0aac
FlorenciaCastelucci <florcastelucci97@gmail.com>
2024-05-28 22:51:02 -0300

    Agrego validaciones y formularios reactivos en ts y html

--- Proyecto 2024/frontend/src/app/pages/cart/checkout/checkout.component.ts
+++ Proyecto 2024/frontend/src/app/pages/cart/checkout/checkout.component.ts
@@ -1,14 +1,54 @@
 import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [RouterLink,RouterOutlet],
+  imports: [RouterLink,RouterOutlet, ReactiveFormsModule],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css'
 })
 export class CheckoutComponent {
+  form!: FormGroup;
+  constructor(private _formBuilder: FormBuilder) {
+    this.form = this._formBuilder.group({
+      name: ['', Validators.required],
+      cardNumber: ['', [Validators.required, Validators.minLength(16), Validators.maxLength(16)]],
+      expiration: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(5)]],
+      cvv: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(3)]],
+  });
+  }
+  
 
+  onEnviar(event:Event)
+  {
+    console.log(this.form.value)
+
+
+    event.preventDefault;
+    if(this.form.valid){
+      alert("Finalizando compra...");
+    }
+    else
+    {
+      this.form.markAllAsTouched()
+    }
+  }
+
+  get Name(){
+    return this.form.get("name")
+  }
+
+  get Cardnumber(){
+    return this.form.get("cardNumber")
+  }
+
+  get Expiration(){
+    return this.form.get("expiration")
+  }
+  get Cvv(){
+    return this.form.get("cvv")
+  }
 }
