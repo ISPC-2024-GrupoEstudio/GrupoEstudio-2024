40d499948368ec97b2e3aa578cd3a4d91f88eaa2
florencia <florencianoelcarrillo@gmail.com>
2024-06-01 18:28:26 -0300

    se crean servicios de angular

--- Proyecto 2024/backend/PetBoutique/PetBoutiqueApp/views.py
+++ Proyecto 2024/backend/PetBoutique/PetBoutiqueApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import viewsets
+from .models import Productos
+from .serializer import ProductosSerializer
+
+class ProductosViewSet(viewsets.ModelViewSet):
+    queryset = Productos.objects.all()
+    serializer_class = ProductosSerializer  
+
