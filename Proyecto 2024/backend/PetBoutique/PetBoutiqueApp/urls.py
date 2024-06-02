6076a5568a307aac6f873999a13b4fc57eb83db6
Markitohh <marcoscordobaa.mc@gmail.com>
2024-06-02 18:31:40 -0300

    Manipulación de roles

--- Proyecto 2024/backend/PetBoutique/PetBoutiqueApp/urls.py
+++ Proyecto 2024/backend/PetBoutique/PetBoutiqueApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 from PetBoutiqueApp import views
+from .views import RoleListCreateAPIView, RoleRetrieveUpdateDestroyAPIView
 
 router=routers.DefaultRouter()
 router.register(r'productos', views.ProductoViewSet)
@@ -9,5 +10,7 @@ router.register(r'proveedores', views.ProveedorViewSet),
 router.register(r'pedidos', views.PedidoViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('roles/', RoleListCreateAPIView.as_view(), name='role-list-create'),
+    path('roles/<int:pk>/', RoleRetrieveUpdateDestroyAPIView.as_view(), name='role-retrieve-update-destroy')
 ]
