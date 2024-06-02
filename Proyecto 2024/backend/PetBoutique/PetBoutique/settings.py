09f5cca16715042272e062fb9d38f9bf7e406c03
emir-argcoder <ea.samsam@gmail.com>
2024-06-02 03:22:56 -0300

    Incorporación de templates y vistas de autenticación de usuarios

--- Proyecto 2024/backend/PetBoutique/PetBoutique/settings.py
+++ Proyecto 2024/backend/PetBoutique/PetBoutique/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -64,7 +65,10 @@ ROOT_URLCONF = 'PetBoutique.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'), # Ruta a las plantillas de Django
+            os.path.join(BASE_DIR, 'frontend') # Ruta a las plantillas de Angular
+            ], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -88,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'pet_boutique',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'Atenas00',
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
@@ -133,8 +137,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
+
