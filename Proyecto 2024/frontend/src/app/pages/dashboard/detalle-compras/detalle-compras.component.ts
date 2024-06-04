9e646b96c0fc55c8cd7602e90befa83eb4f6c736
valenblls18 <valenballada12@gmail.com>
2024-06-03 22:56:24 -0300

    modificaciones en detalles de compras

--- Proyecto 2024/frontend/src/app/pages/dashboard/detalle-compras/detalle-compras.component.ts
+++ Proyecto 2024/frontend/src/app/pages/dashboard/detalle-compras/detalle-compras.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterOutlet} from '@angular/router';
+import { PedidosService } from '../../../services/pedidos.service';
+import { IPedido } from '../../../models/pedido.interface';
+import { NgFor } from '@angular/common';
+import { ProductoXPedidoService } from '../../../services/productoXPedido.service';
+import { IProductoXPedido } from '../../../models/productosXPedido.interface';
+
 
 @Component({
   selector: 'app-detalle-compras',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet, NgFor],
   templateUrl: './detalle-compras.component.html',
-  styleUrl: './detalle-compras.component.css'
+  styleUrl: './detalle-compras.component.css',
+  providers: [PedidosService, ProductoXPedidoService]
 })
-export class DetalleComprasComponent {
 
+export class DetalleComprasComponent implements OnInit{
+  historialPedidos: IPedido[] = [];
+  productoXpedido: IProductoXPedido [] =[]
+
+  constructor(
+    private router: Router, 
+    private pedidoService: PedidosService,
+    private productoXPedidoService: ProductoXPedidoService
+  ) {}
+
+  ngOnInit(): void {
+    this.pedidoService.getPedidos().subscribe((data: IPedido[]) => {
+      this.historialPedidos = data;
+
+    });
+  }
+
+  volverAComprar(idPedido: number): void {
+    // Lógica para volver a comprar
+    console.log(`Volver a comprar el pedido con id: ${idPedido}`);
+  }
 }
+
+
