834cd5610a04a8f204b11149592a1d8cb153b66b
Florencia Castelucci <florcastelucci97@gmail.com>
2025-05-06 19:25:22 -0300

    Refresh token funcionando

--- Proyecto 2024/frontend/src/app/app.component.ts
+++ Proyecto 2024/frontend/src/app/app.component.ts
@@ -12,12 +12,23 @@ import { MinicartComponent } from './pages/cart/minicart/minicart.component';
 import { CheckoutComponent } from './pages/cart/checkout/checkout.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
 import { EditarPerfilComponent } from './pages/perfil/editar-perfil/editar-perfil.component';
+import { AuthService } from './services/auth.service';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './services/auth.interceptor';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavComponent, FooterComponent, DashboardComponent, RegistroComponent, CommonModule,HomeComponent,RouterModule, ProductosComponent, ContactoComponent, MinicartComponent, CheckoutComponent, PerfilComponent, EditarPerfilComponent],
+  providers: [
+    AuthService,  // Proveer AuthService aquí
+    {
+      provide: HTTP_INTERCEPTORS,
+      useValue: AuthInterceptor,  // Registrar el AuthInterceptor como función
+      multi: true  // Permite que se puedan agregar múltiples interceptores
+    }
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Proyecto 2024/frontend/src/app/app.config.ts
+++ Proyecto 2024/frontend/src/app/app.config.ts
@@ -1,9 +1,13 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { withInterceptors } from '@angular/common/http';
 
 import {routes} from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { AuthInterceptor } from './services/auth.interceptor';	
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()]
+  providers: [provideRouter(routes), provideHttpClient(withInterceptors([AuthInterceptor])  
+  )
+]
 };
