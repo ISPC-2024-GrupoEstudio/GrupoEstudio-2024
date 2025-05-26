8afa1f058079b547e53df067b8ecf41f8a1c7397
Milena Gimenez <milenanicolegimenez@gmail.com>
2025-05-26 00:40:44 -0300

    agregando componente politica de privacidad

---
+++ Proyecto 2024/frontend/src/app/pages/politica-privacidad/politica-privacidad.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-politica-privacidad',
+  standalone: true,
+  imports: [],
+  templateUrl: './politica-privacidad.component.html',
+  styleUrl: './politica-privacidad.component.css'
+})
+export class PoliticaPrivacidadComponent {
+  
+}
